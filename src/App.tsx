f19cd903259a51c304dfa86df9a6bf9ffb218997
Camerin <figueroa0609@protonmail.com>
2022-02-09 15:05:14 -0500

    Added Userlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './css/App.css';
 import Messages from './components/Messages';
+import UserList from './components/UserList';
 
 function App() {
+  let users = ["Bob", "Sean","Bob", "Sean","Bob", "Sean","Bob", "Sean","Bob", "Sean", "Cheeseits"];
+
   return (
     <div className="App">
       <div className="col W-20">
-        <div>Users:</div>
-        <div>user</div>
-        <div>Bob</div>
+        <UserList users={users}/>
       </div>
       <div className="col W-80">
         <Messages />
---
+++ src/components/UserList.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react';
+import '../css/UserList.css';
+
+type Props = {
+    users: Array<string>
+}
+
+const UserList: FC<Props> = ({children, users}) => {
+    const list = users.map((user) => {
+        //<User name={name}/>
+        return <div className='user' key={user}>{user}</div>;
+    });
+
+    return (
+        <div className="UserList">
+            <div className="title">Online</div>
+            <div className="list">
+                {list}
+            </div>
+        </div>
+    );
+}
+
+export default UserList;
